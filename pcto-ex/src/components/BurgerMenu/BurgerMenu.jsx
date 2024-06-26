119c2a11466254e31067f68f0e3d458044ff9dbd
riccardo-girone <riccardoo.gironee@gmail.com>
2024-06-26 13:50:19 +0200

    feat: menu setup

--- pcto-ex/src/components/BurgerMenu/BurgerMenu.jsx
+++ pcto-ex/src/components/BurgerMenu/BurgerMenu.jsx
@@ -1,9 +1,26 @@
+'use client'
+
 import styles from './BurgerMenu.module.scss'
+import { useState, useRef, useEffect } from 'react'
+import Link from 'next/link'
+
+const link = [
+    { path: "/intro1", label: "INTRO" },
+    { path: "/cono&cons1", label: "CONO. & CONS." },
+    { path: "/conclusioni1", label: "CONCLUSIONI" },
+]
 
 const BurgerMenu = () => {
-    return(
-        <div>
+    const container = useRef()
+    const [isMenuOpen, setIsMenuOpen] = useState(false)
 
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen)
+    }
+
+    return(
+        <div className={styles.menuContainer} ref={container}>
+           
         </div>
     )
 }
