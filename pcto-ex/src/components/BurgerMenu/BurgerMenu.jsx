4cd843be1fbbda2eba3acb40303bc44c696a3a64
riccardo-girone <149433192+riccardo-girone@users.noreply.github.com>
2024-07-21 21:50:46 +0200

    created burger menu

--- pcto-ex/src/components/BurgerMenu/BurgerMenu.jsx
+++ pcto-ex/src/components/BurgerMenu/BurgerMenu.jsx
@@ -1,48 +1,17 @@
 'use client'
 
 import styles from './BurgerMenu.module.scss'
-import { useState, useRef, useEffect } from 'react'
-import Link from 'next/link'
+import { useState } from 'react';
 
-const link = [
-    { path: "/intro1", label: "INTRO" },
-    { path: "/cono&cons1", label: "CONO. & CONS." },
-    { path: "/conclusioni", label: "CONCLUSIONI" },
-]
+export default function Home() {
 
-const BurgerMenu = () => {
-    const container = useRef()
-    const [isMenuOpen, setIsMenuOpen] = useState(false)
+  const [isActive, setIsActive] = useState(false);
 
-    const toggleMenu = () => {
-        setIsMenuOpen(!isMenuOpen)  
-    }
-
-    return(
-        <div className={styles.menuContainer} ref={container}>
-           <div className={styles.menuOpen} onClick={toggleMenu}>
-                <p>open</p>
-           </div>
-
-           <div className={styles.menuOverlay}>
-                <div className={styles.menuClose}>
-                    <p>close</p>
-                </div>
-
-                <div className={styles.menuLinks}>
-                    {link.map((link, index) => (
-                        <div className={styles.menuLinksItem} key={index}>
-                            <div className={styles.menuLinksItemHolder} onClick={toggleMenu} >
-                                <Link href={link.path} className={styles.menuLink}>
-                                    {link.label}
-                                </Link>
-                            </div>
-                        </div>
-                    ))}
-                </div>
-           </div>
-        </div>
-    )
+  return (
+    <>
+    <div onClick={() => {setIsActive(!isActive)}} className={styles.button}>
+      <div className={`${styles.burger} ${isActive ? styles.burgerActive : ""}`}></div>
+    </div>
+    </>
+  )
 }
-
-export default BurgerMenu
