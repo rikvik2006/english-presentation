31c1bef8a18d6997ce406661c2c608b79cef9230
riccardo-girone <149433192+riccardo-girone@users.noreply.github.com>
2024-07-26 17:05:26 +0200

    fix: delited folder in components + added id in cono.js

--- pcto-ex/src/utils/cono.js
+++ pcto-ex/src/utils/cono.js
@@ -1,5 +1,22 @@
 export const cono = [
     {
-
+        id: "lavoro",
+        numero: "01.",
+        titolo: "COSA E' IL LAVORO"
+    },
+    {
+        id: "tecniche",
+        numero: "02.",
+        titolo: "CONOSCENZE TECNICHE"
+    },
+    {
+        id: "linux",
+        numero: "03.",
+        titolo: "LINUX"
+    },
+    {
+        id: "elettronica",
+        numero: "04.",
+        titolo: "ELETTRONICA"
     },
 ]   
