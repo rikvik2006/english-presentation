9ff8835c66160e6e2c71599a6aac65288cbb135e
riccardo-girone <149433192+riccardo-girone@users.noreply.github.com>
2024-07-29 16:34:25 +0200

    feat: added data slideData

--- pcto-ex/src/utils/slidesData.js
+++ pcto-ex/src/utils/slidesData.js
@@ -1,5 +1,56 @@
 export const slidesData = [
     {
-        
+        id: "disfatta",
+        numero: "00.",
+        content: "Il primo giorno è stato molto diverso da come me lo aspettavo, a causa della disorganizzazione dell'azienda. Avevo già dei pregiudizi su questa esperienza e questo evento li ha confermati.",
+        img: "",
+        settimana: "settimana 1",
+        data: "17 / 06 / 2024",
+        commissione: "none"
+    },
+    {
+        id: "primoGiorno",
+        numero: "01.",
+        content: "Il primo giorno ho configurato circa 18 PLC. Prima di tutto, ho imparato a cosa servono e, in generale, come vengono utilizzati. Questa giornata è stata una delle migliori, soprattutto perché mi hanno affidato una grande responsabilità, dato che ogni PLC costava circa 3000€.",
+        img: "",
+        settimana: "settimana 1",
+        data: "18 / 06 / 2024",
+        commissione: "configurazione plc"
+    },
+    {
+        id: "configurazionePc",
+        numero: "02.",
+        content: "Questi due giorni sono stati i migliori! Mi hanno fatto configurare dei PC che poi verranno spediti in tutto il mondo. La parte più bella è stata risolvere i problemi, perché ce ne sono stati tantissimi (non causati da noi ma da un problema di rete interno). Con la configurazione di Linux, e ho imparato molte cose nuove.",
+        img: "",
+        settimana: "settimana 1",
+        data: "19-20 / 06 / 2024",
+        commissione: "configurazione pc"
+    },
+    {
+        id: "noia",
+        numero: "03.",
+        content: "L'ultimo giorno della prima settimana è stato abbastanza noioso. Dato che i problemi erano stati risolti, ho dovuto solo configurare PC per tutto il giorno.",
+        img: "",
+        settimana: "settimana 1",
+        data: "21 / 06 / 2024",
+        commissione: "configurazione pc"
+    },
+    {
+        id: "miniCorso",
+        numero: "04.",
+        content: "ho finito di configurare i pc ed infine ho fatto un mini corso di come funzionano i principali tipi di saldature della plastica",
+        img: "",
+        settimana: "settimana 2",
+        data: "24 / 06 / 2024",
+        commissione: "pc/saldatura"
+    },
+    {
+        id: "ultimiGiorni",
+        numero: "05.",
+        content: "in questi 4 giorni non ho fatto molto, ho fatto solo piccole task, e nel tempo libero ho realizzato il design per questo sito. ",
+        img: "",
+        settimana: "settimana 2",
+        data: "25-27 / 06 / 2024",
+        commissione: "piccole task"
     },
 ]
