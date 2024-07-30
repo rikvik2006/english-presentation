727ea3f3f4b9f7c3a90e801bb9267c7564cae057
riccardo-girone <149433192+riccardo-girone@users.noreply.github.com>
2024-07-30 17:56:11 +0200

    feat: created and fixed logic for slideUrl

--- pcto-ex/src/app/slidePrev/[slideUrl]/page.js
+++ pcto-ex/src/app/slidePrev/[slideUrl]/page.js
@@ -1,23 +1,23 @@
 'use client'
 import styles from './page.module.scss'
 import { useEffect, useState } from 'react'
-import { slide } from '../../../utils/slidesData.js'
+import slideData  from '../../../utils/slidesData'
 
 export default function Page({ params }) {
     const [ slideUrl, setSlideUrl] = useState(null)
 
     useEffect(() => {
-        for (const slideData of slide) {
-            if (slideData.id == params.slideUrl) {
-                setSlideUrl(slideData)
-
+        for (const slide of slideData) {
+            console.log(params.slide)
+            if (slide.id == params.slideUrl) {
+                setSlideUrl(slide)
                 break
             } 
         }
     }, [])
 
 
-    return <div>{slideUrl?.numero}</div>
+    return <div className={styles.prova}>{slideUrl?.numero}</div>
        
     
 }
