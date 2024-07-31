b6397ce8efa8ce0743198d59c0cdd1fb1c92be40
riccardo-girone <149433192+riccardo-girone@users.noreply.github.com>
2024-07-31 23:34:26 +0200

    feat: created skeleton for slideUrl page

--- pcto-ex/src/app/slidePrev/[slideUrl]/page.js
+++ pcto-ex/src/app/slidePrev/[slideUrl]/page.js
@@ -19,37 +19,37 @@ export default function Page({ params }) {
 
     return (
         <div className={styles.pageContent}>
-            <div className={styles.numero}>
-                {slideUrl?.numero}
+            <div className={styles.numeroWrapper}>
+                <div className={styles.numero}>
+                    {slideUrl?.numero}
+                </div>
             </div>
 
             <div className={styles.intro}>
                 {slideUrl?.intro}
-
             </div>
 
             <div className={styles.mainContent}>
                 <div className={styles.text}>
-
+                    <p>{slideUrl?.content}</p>
                 </div>
 
                 <div className={styles.img}>
-
+                    
                 </div>
             </div>
 
             <div className={styles.footer}>
                 <div className={styles.settimana}>
-
+                    <p>{slideUrl?.settimana}</p>
                 </div>
 
                 <div className={styles.data}>
-
+                    <p>{slideUrl?.data}</p>
                 </div>
 
-
                 <div className={styles.topic}>
-
+                    <p>{slideUrl?.topic}</p>
                 </div>
 
             </div>
