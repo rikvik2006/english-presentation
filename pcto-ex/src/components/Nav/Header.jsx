068c0d7650652492b7695e1c38572aec5d5c7e92
riccardo-girone <149433192+riccardo-girone@users.noreply.github.com>
2024-08-13 21:09:51 +0200

    feat: created nav menu animation

--- pcto-ex/src/components/Nav/Header.jsx
+++ pcto-ex/src/components/Nav/Header.jsx
@@ -2,6 +2,19 @@
 import styles from './Index.module.scss'
 import Buttom from './Button/Index'
 import { useState } from 'react'
+import { motion } from "framer-motion"
+
+const variants = {
+  open: {
+    width: "16vw",
+    height: "23vw"
+  },
+
+  closed: {
+    width: "3.8vw",
+    height: "1.4vw"
+  }
+}
 
 export default function Home() {
 
@@ -9,6 +22,14 @@ export default function Home() {
 
   return (
     <div className={styles.header}>
+        <motion.div 
+          className={styles.menu}
+          variants={variants}
+          animate={isActive ? "open" : "closed"}
+          initial={"closed"}
+        >
+
+        </motion.div>
         <Buttom isActive={isActive} setIsActive={setIsActive}/>
     </div>
   )
