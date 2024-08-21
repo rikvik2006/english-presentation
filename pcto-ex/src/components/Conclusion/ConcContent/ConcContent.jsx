ff35078e9e51ea84b5eb24ecfbccd8bdd3317056
riccardo-girone <149433192+riccardo-girone@users.noreply.github.com>
2024-08-21 15:12:35 +0200

    fix: fixed building error

--- pcto-ex/src/components/Conclusion/ConcContent/ConcContent.jsx
+++ pcto-ex/src/components/Conclusion/ConcContent/ConcContent.jsx
@@ -6,7 +6,7 @@ const ConcContent = () => {
             <div className={styles.text}>
                 Sono contento di questa esperienza. Anche se ho fatto solo lavori di base, 
                 erano comunque lavori veri e non semplici progettini che poi nessuno considera. 
-                L'ambiente era fantastico: tutti scherzavano ed erano gentili, ma lavoravano anche duramente, 
+                L&apos;ambiente era fantastico: tutti scherzavano ed erano gentili, ma lavoravano anche duramente, 
                 creando un clima davvero piacevole.
             </div>
         </div>
