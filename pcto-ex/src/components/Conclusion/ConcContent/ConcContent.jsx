7b2ec03600dfb03264146ba2beefca9b863d8249
riccardo-girone <149433192+riccardo-girone@users.noreply.github.com>
2024-08-22 22:38:44 +0200

    fix: fixed conclusion layout

--- pcto-ex/src/components/Conclusion/ConcContent/ConcContent.jsx
+++ pcto-ex/src/components/Conclusion/ConcContent/ConcContent.jsx
@@ -2,12 +2,20 @@ import styles from './ConcContent.module.scss'
 
 const ConcContent = () => {
     return(
-        <div className={styles.content}>
-            <div className={styles.text}>
-                Sono contento di questa esperienza. Anche se ho fatto solo lavori di base, 
-                erano comunque lavori veri e non semplici progettini che poi nessuno considera. 
-                L&apos;ambiente era fantastico: tutti scherzavano ed erano gentili, ma lavoravano anche duramente, 
-                creando un clima davvero piacevole.
+        <div className={styles.contentWrapper}>
+            <div className={styles.navContent}>
+                <div className={styles.title}>
+                    conclusioni
+                </div>
+            </div>
+
+            <div className={styles.content}>
+                <div className={styles.text}>
+                    Sono contento di questa esperienza. Anche se ho fatto solo lavori di base, 
+                    erano comunque lavori veri e non semplici progettini che poi nessuno considera. 
+                    L&apos;ambiente era fantastico: tutti scherzavano ed erano gentili, ma lavoravano anche duramente, 
+                    creando un clima davvero piacevole.
+                </div>
             </div>
         </div>
     )
--- pcto-ex/src/components/Conclusion/Conclusion.jsx
+++ pcto-ex/src/components/Conclusion/Conclusion.jsx
@@ -1,5 +1,4 @@
 import styles from './Conclusion.module.scss'
-import Nav from './Nav/Nav'
 import Content from './ConcContent/ConcContent'
 import ProContro from './ProContro/ProContro'
 import Utile from './Utile/Utile'
@@ -7,8 +6,6 @@ import Utile from './Utile/Utile'
 const Conclusion = () => {
     return(
         <div className={styles.ContentWrapper}>
-            <Nav/>
-
             <Content/>
 
             <ProContro/>
