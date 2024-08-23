49b77ef130165752c2dc84e48e8c31adb073a887
riccardo-girone <149433192+riccardo-girone@users.noreply.github.com>
2024-08-23 14:11:39 +0200

    fix: fixed text not responsive in nav button

--- pcto-ex/src/components/Conclusion/ConcContent/ConcContent.jsx
+++ pcto-ex/src/components/Conclusion/ConcContent/ConcContent.jsx
@@ -1,15 +1,59 @@
 import styles from './ConcContent.module.scss'
+import gsap from 'gsap'
 
 const ConcContent = () => {
+
+    const initialStates = () => {
+        gsap.set("#navContent", {
+            yPercent: 100,
+        })
+        
+        gsap.set("#content", {
+            yPercent: 100,
+        })
+    }
+    
+    const uiAnimations = () => {
+        const tl = gsap.timeline({
+            delay: 1,
+            
+            defaults: {
+                ease: 'power3.out',
+                duration: 1,
+                yPercent: 0,
+                y: 0
+            }
+            
+        })
+        
+        tl.to("#navContent", {
+            
+        })
+        
+        .to("#content", {
+            
+        }, ".6")
+        
+        return tl
+    }
+    
+    
+    const master = gsap.timeline()
+    master
+    .add(initialStates())
+    .add(uiAnimations())
+
+
+
     return(
         <div className={styles.contentWrapper}>
-            <div className={styles.navContent}>
+            <div id="navContent" className={styles.navContent}>
                 <div className={styles.title}>
                     conclusioni
                 </div>
             </div>
 
-            <div className={styles.content}>
+            <div id="content"  className={styles.content}>
                 <div className={styles.text}>
                     Sono contento di questa esperienza. Anche se ho fatto solo lavori di base, 
                     erano comunque lavori veri e non semplici progettini che poi nessuno considera. 
